a79fb8d11f05751df6b032a1cd826b5d8f186353
Andrey Andreev <andrey.andreev303@gmail.com>
2019-06-22 09:23:38 +0300

    Replace existing css with styled-components

---
+++ src/AppStyled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const AppStyled = styled.div`
+  text-align: center;
+`
+
+export const HeaderStyled = styled.header`
+  background-color: #282c34;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
